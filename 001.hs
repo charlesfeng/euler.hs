85ebcdfcaeefcdfef562eadb15f2663431d28a8a
Charles Feng <charles@charles.io>
2013-06-07 16:58:03 -0700

    add spaces

--- 001.hs
+++ 001.hs
@@ -1,10 +1,10 @@
---project euler: problem 1 (http://projecteuler.net/problem=1)
+-- project euler: problem 1 (http://projecteuler.net/problem=1)
 
---(c) 2013 charles feng (https://github.com/charlesfeng)
---shared under the mit license (http://www.opensource.org/licenses/mit)
+-- (c) 2013 charles feng (https://github.com/charlesfeng)
+-- shared under the mit license (http://www.opensource.org/licenses/mit)
 
 main = do
   print $ sum $ filter (\x -> x `mod` 3 == 0 || x `mod` 5 == 0) [1 .. 999]
 
---answer: 233168
---runtime: 0.2s
+-- answer: 233168
+-- runtime: 0.2s
--- 002.hs
+++ 002.hs
@@ -1,12 +1,12 @@
---project euler: problem 2 (http://projecteuler.net/problem=2)
+-- project euler: problem 2 (http://projecteuler.net/problem=2)
 
---(c) 2013 charles feng (https://github.com/charlesfeng)
---shared under the mit license (http://www.opensource.org/licenses/mit)
+-- (c) 2013 charles feng (https://github.com/charlesfeng)
+-- shared under the mit license (http://www.opensource.org/licenses/mit)
 
 f = 1 : 2 : zipWith (+) f (tail f)
 
 main = do
   print $ sum $ filter even $ takeWhile (<= 4000000) f
 
---answer: 4613732
---runtime: 0.2s
+-- answer: 4613732
+-- runtime: 0.2s
