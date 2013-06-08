2e0556f67a55ffc32bba76ff50e82bd15f08572d
Charles Feng <charles@charles.io>
2013-06-08 01:01:47 -0700

    make primes slightly more efficient by guarding

--- modules/Primes.hs
+++ modules/Primes.hs
@@ -3,12 +3,11 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
-module Primes
-( primes
-) where
+module Primes (primes) where
 
 import Minus
 
 primes n = 2 : s [3, 5 .. n] where
-  s []      = []
-  s (p : x) = p : s (x `minus` [p * p, p * p + 2 * p .. n])
+  s (p : x)
+    | p * p > n  = p : x
+    | otherwise  = p : s (x `minus` [p * p, p * p + 2 * p ..])
