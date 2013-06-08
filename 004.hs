d908cb1fc88d57bdb7e15dbf298e8d434f36b8ff
Charles Feng <charles@charles.io>
2013-06-07 18:11:33 -0700

    start list at 800

--- 004.hs
+++ 004.hs
@@ -4,7 +4,7 @@
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
 main = do
-  print $ maximum [x * y | x <- [100 .. 999], y <- [100 .. 999], show(x * y) == reverse(show(x * y))]
+  print $ maximum [x * y | x <- [800 .. 999], y <- [800 .. 999], show(x * y) == reverse(show(x * y))]
 
 -- answer: 906609
--- runtime: 2.6s
+-- runtime: 0.3s
