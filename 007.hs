2c192fc364dd133ba60dda7a99498b1fbe9af67c
Charles Feng <charles@charles.io>
2013-06-08 01:02:13 -0700

    problem 7

---
+++ 007.hs
@@ -0,0 +1,12 @@
+-- project euler: problem 7 (http://projecteuler.net/problem=7)
+
+-- (c) 2013 charles feng (https://github.com/charlesfeng)
+-- shared under the mit license (http://www.opensource.org/licenses/mit)
+
+import Primes
+
+main = do
+  print $ primes 120000 !! 10000
+
+-- answer: 104743
+-- runtime: 1.1s
