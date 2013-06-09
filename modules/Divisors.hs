888fa6aa5110808a45fe70d54dd99de1e79ca5e6
Charles Feng <charles@charles.io>
2013-06-08 19:21:46 -0700

    use ' for proper divisor fxns

--- modules/Divisors.hs
+++ modules/Divisors.hs
@@ -3,7 +3,7 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
-module Divisors (numDivisors, numProperDivisors, sumDivisors, sumProperDivisors) where
+module Divisors (numDivisors, numDivisors', sumDivisors, sumDivisors') where
 
 import Factor
 import Data.List
@@ -11,11 +11,11 @@ import Data.List
 numDivisors :: Int -> Int
 numDivisors n = product $ map (\x -> 1 + length x) $ group $ pfactor n
 
-numProperDivisors :: Int -> Int
-numProperDivisors n = numDivisors n - 1
+numDivisors' :: Int -> Int
+numDivisors' n = numDivisors n - 1
 
 sumDivisors :: Int -> Int
 sumDivisors n = product $ map (\x -> ((head x) ^ (1 + length x) - 1) `div` (head x - 1)) $ group $ pfactor n
   
-sumProperDivisors :: Int -> Int
-sumProperDivisors n = sumDivisors n - n
+sumDivisors' :: Int -> Int
+sumDivisors' n = sumDivisors n - n
