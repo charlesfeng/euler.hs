cb52de556dc700a26ae07752df2234565b991a65
Charles Feng <charles@charles.io>
2013-06-09 16:31:53 -0700

    don't need , 2

--- 012.hs
+++ 012.hs
@@ -9,7 +9,7 @@ import Divisors
 ps = primes 10000000
 
 main = do
-  print $ head $ [ y | x <- [1, 2 ..], let y = x * (x + 1) `div` 2, numDivisors' y ps > 500 ]
+  print $ head $ [ y | x <- [1 ..], let y = x * (x + 1) `div` 2, numDivisors' y ps > 500 ]
 
 -- answer: 76576500
 -- runtime: 3.3s
