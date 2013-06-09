a3891c0d23e2e68a4fb5ed5f5022d045b54742df
Charles Feng <charles@charles.io>
2013-06-08 18:26:51 -0700

    pfactor function

--- modules/Factor.hs
+++ modules/Factor.hs
@@ -3,7 +3,9 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
-module Factor (factor) where
+module Factor (factor, pfactor) where
+
+import Primes
   
 factor :: Int -> [Int]
 factor n = factor' n 2
@@ -13,3 +15,13 @@ factor' n f
   | f * f > n       = [n]
   | n `mod` f == 0  = f : factor' (n `div` f) 2
   | otherwise       = factor' n (f + 1)
+  
+pfactor :: Int -> [Int]
+pfactor n = pfactor' n 0 (primes n)
+
+pfactor' :: Int -> Int -> [Int] -> [Int]
+pfactor' n i p
+  | f * f > n       = [n]
+  | n `mod` f == 0  = f : pfactor' (n `div` f) 0 p
+  | otherwise       = pfactor' n (i + 1) p
+  where f = p !! i
