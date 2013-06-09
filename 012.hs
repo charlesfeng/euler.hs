f2abe325e0b87aab5315844062c6673c7d8db704
Charles Feng <charles@charles.io>
2013-06-08 19:36:09 -0700

    make problem 12 >20x faster

--- 012.hs
+++ 012.hs
@@ -3,10 +3,13 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
+import Primes
 import Divisors
 
+ps = primes 10000000
+
 main = do
-  print $ head $ [ y | x <- [1, 2 ..], let y = x * (x + 1) `div` 2, numDivisors y > 500 ]
+  print $ head $ [ y | x <- [1, 2 ..], let y = x * (x + 1) `div` 2, numDivisors' y ps > 500 ]
 
 -- answer: 76576500
--- runtime: 77.6s
+-- runtime: 3.3s
