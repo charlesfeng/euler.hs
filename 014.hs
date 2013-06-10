0db955d38b1375ec4ee8f8cb0da83499d964b280
Charles Feng <charles@charles.io>
2013-06-09 17:04:07 -0700

    list memoization - still too slow

--- 014.hs
+++ 014.hs
@@ -2,9 +2,17 @@
 
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
+  
+cz = (map cz' [0 .. ] !!)
+  where
+    cz' n
+      | n == 1          = 1
+      | n > 999999      = _cz n
+      | n `mod` 2 == 0  = 1 + (cz $ n `div` 2)
+      | otherwise       = 1 + (cz $ 3 * n + 1)
 
 _cz n
-  | n == 1          = 1
+  | n < 1000000       = cz n
   | n `mod` 2 == 0  = 1 + (_cz $ n `div` 2)
   | otherwise       = 1 + (_cz $ 3 * n + 1)
 
