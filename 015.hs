1cf39c22b3c28439e485af7782867502f2462437
Charles Feng <charles@charles.io>
2013-06-09 18:03:03 -0700

    .. use product for factorial

--- 015.hs
+++ 015.hs
@@ -3,10 +3,8 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
-fs = scanl1 (*) [1 ..]
-
 main = do
-  print $ fs !! 39 `div` (fs !! 19) ^ 2
+  print $ product [1 .. 39] `div` (product [1 .. 19]) ^ 2
 
 -- answer: 137846528820
 -- runtime: 0.3s
