9c2dce5a80758c6e661c1eea7f7f0220e8181407
Charles Feng <charles@charles.io>
2013-06-09 18:01:52 -0700

    use digitToInt function

--- 016.hs
+++ 016.hs
@@ -3,12 +3,10 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
-sd n
-  | n < 10     = n
-  | otherwise  = (n `mod` 10) + sd (n `div` 10)
+import Data.Char
 
 main = do
-  print $ sd $ 2 ^ 1000
+  print $ sum . map digitToInt . show $ 2 ^ 1000
 
 -- answer: 1366
 -- runtime: 0.3s
