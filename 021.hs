7084ce4ac540a907ce22a7de7edecf4667e5d5c5
Charles Feng <charles@charles.io>
2013-06-10 00:32:43 -0700

    use arrays to make it easier to read/faster

--- 021.hs
+++ 021.hs
@@ -3,21 +3,22 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
+import Data.Array
 import Primes
 import Divisors
 
 ps = primes 9999
-ds = map (\x -> sumProperDivisors' x ps) [2 .. 9999]
+ds = array (2, 9999) [ (x, sumProperDivisors' x ps) | x <- [2 .. 9999] ]
 
 main = do
   print $ foldl (\a x -> 
-    if ds !! (x - 2) > 1
-      && ds !! (x - 2) < 10000
-      && x /= ds !! (x - 2)
-      && x == ds !! (ds !! (x - 2) - 2)
+    if ds ! x > 1
+      && ds ! x < 10000
+      && x /= ds ! x
+      && x == ds ! (ds ! x)
       then a + x 
       else a
     ) 0 [2 .. 9999]
 
 -- answer: 31626
--- runtime: 2.7s
+-- runtime: 1.0s
