f1d2bee0779cf173a699b7f861639af5b1631fa4
Charles Feng <charles@charles.io>
2013-06-10 01:21:06 -0700

    problem 23

---
+++ 023.hs
@@ -0,0 +1,19 @@
+-- project euler: problem 23 (http://projecteuler.net/problem=23)
+
+-- (c) 2013 charles feng (https://github.com/charlesfeng)
+-- shared under the mit license (http://www.opensource.org/licenses/mit)
+
+import Data.List
+import Data.IntSet (toList, fromList)
+import Primes
+import Divisors
+
+ps   = primes 28123
+ab   = filter (\x -> x < sumProperDivisors' x ps) [2 .. 28122]
+sums = [ x + y | x <- ab, y <- ab, x <= y, x + y <= 28123 ]
+
+main = do
+  print $ (28123 * 28124 `div` 2) - (sum . toList . fromList) sums
+
+-- answer: 4179871
+-- runtime: 30.2s
