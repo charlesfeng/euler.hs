23177ef04fd36c10727c7ec9a6f36a697fc73874
Charles Feng <charles@charles.io>
2013-06-10 21:55:07 -0700

    problem 37

---
+++ 037.hs
@@ -0,0 +1,20 @@
+-- project euler: problem 37 (http://projecteuler.net/problem=37)
+
+-- (c) 2013 charles feng (https://github.com/charlesfeng)
+-- shared under the mit license (http://www.opensource.org/licenses/mit)
+
+import Data.Set (fromList, member)
+import Primes
+
+ps    = map (show) $ primes 1000000
+pss   = fromList ps
+
+cir x = all (\x -> member x pss) $ lr x ++ rr x
+  where lr x = [ drop r $ x | r <- [0 .. length x - 1] ]
+        rr x = [ reverse $ drop r $ reverse x | r <- [0 .. length x - 1] ]
+
+main = do
+  print $ sum [ read x | x <- drop 4 ps, cir x ]
+
+-- answer: 748317
+-- runtime: 11.4s
