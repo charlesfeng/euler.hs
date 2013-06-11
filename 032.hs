4027c79328eea78cb76f590bd09c86c29cce45c7
Charles Feng <charles@charles.io>
2013-06-10 20:05:50 -0700

    don't need Data.Char

--- 032.hs
+++ 032.hs
@@ -3,7 +3,6 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
-import Data.Char
 import Data.List
 
 pan a b = length d == 9 && (length . nub) d == length d && not ('0' `elem` d)
