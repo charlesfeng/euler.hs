fa860d98c5f89eae2ddf7f73ce20af39901a06f0
Charles Feng <charles@charles.io>
2013-06-10 19:10:33 -0700

    problem 32

---
+++ 032.hs
@@ -0,0 +1,21 @@
+-- project euler: problem 32 (http://projecteuler.net/problem=32)
+
+-- (c) 2013 charles feng (https://github.com/charlesfeng)
+-- shared under the mit license (http://www.opensource.org/licenses/mit)
+
+import Data.Char
+import Data.List
+
+pan a b = length d == 9 && (length . nub) d == length d && not ('0' `elem` d)
+  where c  = a * b
+        d = show a ++ show b ++ show c
+
+main = do
+  print $ sum $ nub $
+    [ x * y
+    | x <- [1 .. 98]
+    , y <- [123 .. 4987]
+    , pan x y ]
+
+-- answer: 45228
+-- runtime: 1.2s
