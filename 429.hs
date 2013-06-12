992c130ec8801f09944cec727b47a2a10106a34d
Charles Feng <charles@charles.io>
2013-06-12 01:13:45 -0700

    make it one line shorter

--- 429.hs
+++ 429.hs
@@ -10,9 +10,8 @@ import Primes
 
 main = do
   print $ foldl' (\a x -> (a * x) `mod` m) 1 [ 1 + modPow p (2 * factorCount n p) m | p <- ps ]
-  where n = 100000000
-        m = 1000000009
-        ps = primes n
+  where (n, m) = (100000000, 1000000009)
+        ps     = primes n
 
 -- answer: 98792821
 -- runtime: 156.3s
