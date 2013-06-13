592a9c86269583468722d6653a1d699ed45da42a
Charles Feng <charles@charles.io>
2013-06-13 00:53:52 -0700

    split into multiple lines

--- 049.hs
+++ 049.hs
@@ -9,7 +9,13 @@ import Primes
 import Perms
 
 main = do
-  putStrLn $ last $ [ (show $ n !! x) ++ (show $ n !! y) ++ (show $ n !! z) | n <- lps, x <- [0 .. length n - 3], y <- [x + 1 .. length n - 2], z <- [y + 1 .. length n - 1], n !! z - n !! y == n !! y - n !! x ]
+  putStrLn $ last $
+    [ (show $ n !! x) ++ (show $ n !! y) ++ (show $ n !! z)
+    | n <- lps
+    , x <- [0 .. length n - 3]
+    , y <- [x + 1 .. length n - 2]
+    , z <- [y + 1 .. length n - 1]
+    , n !! z - n !! y == n !! y - n !! x ]
     where ps  = filter (>= 1000) $ primes 9999
           sps = fromList ps
           lps = filter (\a -> length a > 2) $ nub [ sort $ toList $ sps `intersection` (fromList . filter (> 1000) . map (\a -> read a :: Int) . (perms 4) . show) x | x <- ps ]
