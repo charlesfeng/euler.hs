06426172222fcc7121644fbb7f97c74676a7333b
Charles Feng <charles@charles.io>
2013-06-24 00:21:07 -0700

    add another solution for 26 using 9's

--- 026.hs
+++ 026.hs
@@ -8,7 +8,11 @@ import Data.List
 import Data.Ord
 
 main = do
+  -- method 1, using Fermat (runtime 0.3s)
   print $ fst $ maximumBy (comparing snd) [ head [ (x, y) | y <- [1 .. x - 1], (10 ^ y) `mod` x == 1 ] | x <- map (toInteger) $ drop 3 $ primes 1000 ]
+  
+  -- method 2, using 9's (runtime 3.6s)
+  print $ fst $ maximumBy (comparing snd) [ (x, fst $ head $ dropWhile (\a -> (snd a) `rem` x /= 0) [ (y, read (replicate y '9') :: Integer) | y <- [1 ..] ]) | x <- map (toInteger) $ drop 3 $ primes 1000 ]
 
 -- answer: 983
 -- runtime: 0.3s
