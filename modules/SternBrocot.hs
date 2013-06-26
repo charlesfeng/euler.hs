ecd998eb43e03317bca835b3e35257add12a9381
Charles Feng <charles@charles.io>
2013-06-26 16:29:10 -0700

    half stern-brocot tree (< 1)

--- modules/SternBrocot.hs
+++ modules/SternBrocot.hs
@@ -3,8 +3,13 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
-module SternBrocot (stern) where
+module SternBrocot (stern, halfStern) where
 
 stern :: Int -> [(Int, Int)]
-stern n = init $ tail $ iterate (stern') [(0, 1), (1, 0)] !! n
-  where stern' l = (concat $ map (\a -> [fst a, ((fst . fst) a + (fst . snd) a, (snd . fst) a + (snd . snd) a)]) $ zip l (drop 1 l)) ++ [last l]
+stern n = init $ tail $ iterate (_stern) [(0, 1), (1, 0)] !! n
+
+halfStern :: Int -> [(Int, Int)]
+halfStern n = init $ tail $ iterate (_stern) [(0, 1), (1, 1)] !! n
+
+_stern :: [(Int, Int)] -> [(Int, Int)]
+_stern l = (concat $ map (\a -> [fst a, ((fst . fst) a + (fst . snd) a, (snd . fst) a + (snd . snd) a)]) $ zip l (drop 1 l)) ++ [last l]
