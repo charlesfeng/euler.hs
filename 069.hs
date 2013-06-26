ec7ecd06d2a66b20648254d83c65ef8c342d407b
Charles Feng <charles@charles.io>
2013-06-25 23:03:52 -0700

    make problem 69 faster

--- 069.hs
+++ 069.hs
@@ -3,16 +3,10 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
-import Data.List
-import Data.Ord
-import Data.Ratio
-import Phi
 import Primes
 
-ps = primes 1000000
-
 main = do
-  print $ fst $ maximumBy (comparing snd) [ (x, x % (phi' x ps)) | x <- [1 .. 1000000] ]
+  print $ last . takeWhile (< 1000000) . scanl1 (*) $ primes 1000000
 
 -- answer: 510510
--- runtime: 138.9s
+-- runtime: 0.6s
