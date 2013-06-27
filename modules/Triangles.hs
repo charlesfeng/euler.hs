c6686cebd2792ae1939464a773644f97a3ef37cc
Charles Feng <charles@charles.io>
2013-06-26 18:39:07 -0700

    add infinite pythagorean triple generator

--- modules/Triangles.hs
+++ modules/Triangles.hs
@@ -3,10 +3,13 @@
 -- (c) 2013 charles feng (https://github.com/charlesfeng)
 -- shared under the mit license (http://www.opensource.org/licenses/mit)
 
-module Triangles (pyTriSum, pyTriSum') where
+module Triangles (pyTriSum, pyTriSum', pyTris) where
 
 import Data.List
 import Divisors
+
+pyTris :: [[Int]]
+pyTris = concat [ [ sort [m ^ 2 - n ^ 2, 2 * m * n, m ^ 2 + n ^ 2] | n <- [1 .. m - 1], gcd m n == 1, odd $ m - n ] | m <- [1 ..] ]
 
 pyTriSum :: Int -> [[Int]]  
 pyTriSum s = _pyTriSum s (divisors s)
