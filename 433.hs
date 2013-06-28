a7a69e76af6d43224acef305b8d687347e75ce0a
Charles Feng <charles@charles.io>
2013-06-27 23:39:54 -0700

    o m g

--- 433.hs
+++ 433.hs
@@ -6,23 +6,16 @@
 import Data.Array
 import Data.List
 
-s = 10000
+s = 1000
 
-euc = (listArray ((0, 0), (s, s)) [ ((x, y), euc' x y) | x <- [0 .. s], y <- [0 .. s] ] !)
-  where euc' x y = if y == 0 then 0 else 1 + (snd $ euc (y, x `mod` y))
-
-eucs = foldl' (\a (m, n) -> a + (s `div` m) * ((snd $ euc (m, n)) + (snd $ euc (n, m)))) 0
-
-gen l = concat [ filter (/= (0, 0)) [g1, g2, g3]
-               | (m, n) <- l
-               , let g1 = if 2 * m - n <= s then (2 * m - n, m) else (0, 0)
-               , let g2 = if 2 * m + n <= s then (2 * m + n, m) else (0, 0)
-               , let g3 = if m + 2 * n <= s then (m + 2 * n, n) else (0, 0) ]
+euc x y = a ! (x, y)
+  where a  = a' // [ ((i * x, i * y), e x y) | i <- [1 .. s `div` x] ]
+        a' = listArray ((0, 0), (s, s)) [ ((i, j), snd $ euc i j) | i <- [0 .. s], j <- [0 .. s] ]
+        e x y = if y == 0 then 0 else 1 + (snd $ euc y $ x `mod` y)
 
 main = do
-  print $ s +
-    (foldl' (\a l -> a + eucs l) 0 $ takeWhile (/= []) $ iterate (gen) [(2, 1)]) +
-    (foldl' (\a l -> a + eucs l) 0 $ takeWhile (/= []) $ iterate (gen) [(3, 1)])
+  print $ euc 10 6
+  --print $ 2 * (sum [ snd $ euc (x, y) | x <- [2 .. s], y <- [1 .. x - 1] ]) + (s - 1) * s `div` 2 + s
 
 -- answer: 
 -- runtime: 
